730306f30935329e98840c4270492053b78cf92d
Muhammad Abdul Haseeb <muhammad.abdul.haseeb@xgrid.co>
2022-10-16 22:28:45 +0500

    Adding 1 commit

--- README.md
+++ README.md
@@ -17,3 +17,21 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Lifecycle Methods 
+1- Vue.createApp() => app.mount()
+
+2- [beforeCreate] Hook => 
+- this fires before the component is fully created and its at very start of initilization
+- We cant access data from data object or any template elements yet
+
+3- [created] Hook => 
+- When it creates a component, it fires a created hook
+- The component gets created but not mounted to the dom, So we cant access the data now but we can access the template
+
+4- [beforeMount] Hook =>
+- After this it starts compiling our template and when it does that, it fires the beforeMount Hook
+- In here We can now access our data, events & templates
+
+5- [Mounted] Hook => 
+- 
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="startgame" :disabled="isPlaying">Play</button>
+  <!-- Only Show block only if we are playing that's why we used v-if-->
+  <!-- Binding some data into this because we want to pass in this property v-bind: or only : -->
+  <Block  v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Component imports
+import Block from "./components/Block.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false, // must be true to start the game
+      delay: null, // must be in 20 sec
+    };
+  },
+  methods: {
+    startgame() {
+      this.delay = 2000 + Math.random() * 5000; // Min delay time will be 2000 and max delay time 7000
+      this.isPlaying = true;
+    },
+  },
+};
 </script>
 
 <style>
